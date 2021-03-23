e06dd642779f1bbafb035126932c8386d49150c7
Juha Luoma <33253757+LuomaJuha@users.noreply.github.com>
2021-03-23 12:52:40 +0200

    [ALLI-6895] Fix indicator check in SolrMarc::getStandardCodes (#1960)
    Old version mistakenly considered an empty indicator value as equivalent to 0.

--- module/Finna/src/Finna/RecordDriver/SolrMarc.php
+++ module/Finna/src/Finna/RecordDriver/SolrMarc.php
@@ -1940,22 +1940,22 @@ class SolrMarc extends \VuFind\RecordDriver\SolrMarc
         foreach ($this->getMarcReader()->getFields('024') as $field) {
             $subfields = [];
             switch ($field['i1']) {
-            case 0:
+            case '0':
                 $subfields[] = 'ISRC';
                 break;
-            case 1:
+            case '1':
                 $subfields[] = 'UPC';
                 break;
-            case 2:
+            case '2':
                 $subfields[] = 'ISMN';
                 break;
-            case 3:
+            case '3':
                 $subfields[] = 'EAN';
                 break;
-            case 4:
+            case '4':
                 $subfields[] = 'SICI';
                 break;
-            case 7:
+            case '7':
                 if ($sub2 = $this->getSubfield($field, '2')) {
                     $subfields[] = $this->stripTrailingPunctuation($sub2);
                 }
