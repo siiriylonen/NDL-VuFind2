eb52dbdf18adbcb631759d7c9ec30441344b8ea8
Aleksi Peebles <aleksip@users.noreply.github.com>
2021-06-11 08:56:57 +0300

    Convert to int after null coalescing (#2036)

--- module/Finna/src/Finna/Search/Favorites/Results.php
+++ module/Finna/src/Finna/Search/Favorites/Results.php
@@ -131,7 +131,10 @@ class Results extends \VuFind\Search\Favorites\Results
     public function getListObject()
     {
         $filters = $this->getParams()->getRawFilters();
-        $listId = intval($filters['lists'][0]) ?? null;
+        $listId = $filters['lists'][0] ?? null;
+        if (null !== $listId) {
+            $listId = intval($listId);
+        }
 
         // Load a list when
         //   a. if we haven't previously tried to load a list ($this->list = false)
