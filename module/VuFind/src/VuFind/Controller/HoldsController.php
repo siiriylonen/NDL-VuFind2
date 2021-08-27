1210e862657e6c0163162cbdacfe9f688ebc6ec4
Ere Maijala <ere.maijala@helsinki.fi>
2021-08-27 14:41:02 +0300

    Always unset updateDetails unless the value is usable. (#2077)
    - Previously an empty string would have been kept and used for the id in the list.

--- module/VuFind/src/VuFind/Controller/HoldsController.php
+++ module/VuFind/src/VuFind/Controller/HoldsController.php
@@ -131,15 +131,15 @@ class HoldsController extends AbstractBase
             }
 
             // Add update details if appropriate
-            if (empty($holdConfig['updateFields'])) {
-                if (isset($current['updateDetails'])) {
+            if (isset($current['updateDetails'])) {
+                if (empty($holdConfig['updateFields'])
+                    || '' === $current['updateDetails']
+                ) {
                     unset($current['updateDetails']);
+                } else {
+                    $view->updateForm = true;
+                    $this->holds()->rememberValidId($current['updateDetails']);
                 }
-            } elseif (isset($current['updateDetails'])
-                && '' !== $current['updateDetails']
-            ) {
-                $view->updateForm = true;
-                $this->holds()->rememberValidId($current['updateDetails']);
             }
 
             $driversNeeded[] = $current;
