30b23035bad39bc0ebf6a9200b03e3f3698e9ac6
Juha Luoma <33253757+LuomaJuha@users.noreply.github.com>
2021-11-17 13:46:14 +0200

    [ALLI-7261] Call unveil on linked events images after content is loaded (#2177)

--- themes/finna2/js/finna-linked-events.js
+++ themes/finna2/js/finna-linked-events.js
@@ -29,6 +29,7 @@ finna.linkedEvents = (function finnaLinkedEvents() {
       .done(function onGetEventsDone(response) {
         if (response.data) {
           callback(response.data, append, container);
+          container.find('img').unveil();
         } else {
           var err = $('<div></div>').attr('class', 'linked-events-noresults infobox').text(VuFind.translate('nohit_heading'));
           container.find($('.linked-events-content')).html(err);
