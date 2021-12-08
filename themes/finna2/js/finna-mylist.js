2a0ce6fdc358eb64db802edc5604fbe21c366f1f
Heikki Ala-Nikkola <heikki.ala-nikkola@gofore.com>
2021-12-08 15:41:36 +0200

    [ALLI-7279 ] List notes: Disable collapsible from list views, minor style adjustments (#2193)

--- themes/finna2/js/finna-mylist.js
+++ themes/finna2/js/finna-mylist.js
@@ -386,7 +386,7 @@ finna.myList = (function finnaMyList() {
     // hide/show notes on images
     $('.note-button:not(.inited)').each(function initNotes() {
       var btn = $(this);
-      var noteOverlay = btn.siblings('.note-overlay-grid, .note-overlay-condensed, .note-overlay-list').first();
+      var noteOverlay = btn.siblings('.note-overlay-grid, .note-overlay-condensed').first();
       btn.off('click').on('click', function onClick(e) {
         e.stopPropagation();
         btn.add(noteOverlay).toggleClass('note-show', !btn.hasClass('note-show'));
