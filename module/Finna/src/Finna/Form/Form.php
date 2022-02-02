95346ce6f2a3f342bab03933ad0d9f728a1e3e4e
Ere Maijala <ere.maijala@helsinki.fi>
2022-02-02 14:21:46 +0200

    Fix escaping of html translation parameters.

--- module/Finna/src/Finna/Form/Form.php
+++ module/Finna/src/Finna/Form/Form.php
@@ -508,13 +508,13 @@ class Form extends \VuFind\Form\Form
         if ($this->userCatUsername) {
             $preParagraphs[] = $this->translate(
                 'feedback_library_card_barcode_html',
-                ['%%barcode%%' => $this->userCatUsername]
+                ['%%barcode%%' => $escapeHtml($this->userCatUsername)]
             );
         }
         if ($this->userCatId) {
             $postParagraphs[] = $this->translate(
                 'feedback_library_patron_id_html',
-                ['%%id%%' => $this->userCatId]
+                ['%%id%%' => $escapeHtml($this->userCatId)]
             );
         }
 
