42a6e8dc58fbe07e10511c919507fe3cee7c0c25
Ere Maijala <ere.maijala@helsinki.fi>
2022-08-24 08:30:33 +0300

    Fix RecordDataFormatterFactory to use callables and VuFind's as base.
    Adds upstream parity, avoids duplication of code and parses defaults on demand for better performance.

--- module/Finna/src/Finna/View/Helper/Root/RecordDataFormatterFactory.php
+++ module/Finna/src/Finna/View/Helper/Root/RecordDataFormatterFactory.php
@@ -5,7 +5,7 @@
  * PHP version 7
  *
  * Copyright (C) Villanova University 2016.
- * Copyright (C) The National Library of Finland 2017-2020.
+ * Copyright (C) The National Library of Finland 2017-2022.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2,
@@ -26,12 +26,17 @@
  * @author   Konsta Raunio <konsta.raunio@helsinki.fi>
  * @author   Samuli Sillanpää <samuli.sillanpaa@helsinki.fi>
  * @author   Aleksi Peebles <aleksi.peebles@helsinki.fi>
+ * @author   Ere Maijala <ere.maijala@helsinki.fi>
  * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
  * @link     https://vufind.org/wiki/development:architecture:record_data_formatter
  * Wiki
  */
 namespace Finna\View\Helper\Root;
 
+use Laminas\ServiceManager\Exception\ServiceNotCreatedException;
+use Laminas\ServiceManager\Exception\ServiceNotFoundException;
+use Psr\Container\ContainerExceptionInterface as ContainerException;
+use Psr\Container\ContainerInterface;
 use VuFind\View\Helper\Root\RecordDataFormatter\SpecBuilder;
 
 /**
@@ -43,26 +48,41 @@ use VuFind\View\Helper\Root\RecordDataFormatter\SpecBuilder;
  * @author   Konsta Raunio <konsta.raunio@helsinki.fi>
  * @author   Samuli Sillanpää <samuli.sillanpaa@helsinki.fi>
  * @author   Aleksi Peebles <aleksi.peebles@helsinki.fi>
+ * @author   Ere Maijala <ere.maijala@helsinki.fi>
  * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
  * @link     https://vufind.org/wiki/development:architecture:record_data_formatter
  * Wiki
  */
 class RecordDataFormatterFactory
+    extends \VuFind\View\Helper\Root\RecordDataFormatterFactory
 {
     /**
-     * Create the helper.
+     * Create an object
      *
-     * @return RecordDataFormatter
+     * @param ContainerInterface $container     Service manager
+     * @param string             $requestedName Service being created
+     * @param null|array         $options       Extra options (optional)
+     *
+     * @return object
+     *
+     * @throws ServiceNotFoundException if unable to resolve the service.
+     * @throws ServiceNotCreatedException if an exception is raised when
+     * creating a service.
+     * @throws ContainerException&\Throwable if any other error occurs
+     *
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
      */
-    public function __invoke()
-    {
-        $helper = new RecordDataFormatter();
-        $helper->setDefaults('core', $this->getDefaultCoreSpecs());
-        $helper->setDefaults('description', $this->getDefaultDescriptionSpecs());
-        $helper->setDefaults('authority', $this->getDefaultAuthoritySpecs());
+    public function __invoke(
+        ContainerInterface $container,
+        $requestedName,
+        array $options = null
+    ) {
+        $helper = parent::__invoke($container, $requestedName, $options);
+
+        $helper->setDefaults('authority', [$this, 'getDefaultAuthoritySpecs']);
         $helper->setDefaults(
             'authorityRecommend',
-            $this->getDefaultAuthorityRecommendSpecs()
+            [$this, 'getDefaultAuthorityRecommendSpecs']
         );
         return $helper;
     }
