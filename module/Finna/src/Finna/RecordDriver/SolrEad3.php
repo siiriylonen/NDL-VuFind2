19a16d2a6e98826f2c0df27966f9170bc99bcaa5
Ere Maijala <ere.maijala@helsinki.fi>
2022-09-14 16:30:49 +0300

    SolrEad: Fix several issues where null is passed for string.

--- module/Finna/src/Finna/RecordDriver/SolrEad3.php
+++ module/Finna/src/Finna/RecordDriver/SolrEad3.php
@@ -575,7 +575,7 @@ class SolrEad3 extends SolrEad
             if ($label && !in_array($label, self::UNIT_IDS)) {
                 continue;
             }
-            $displayLabel = null;
+            $displayLabel = '';
             if ($label) {
                 $displayLabel = "Unit ID:$label";
             } elseif ($manyIds) {
@@ -652,7 +652,7 @@ class SolrEad3 extends SolrEad
     /**
      * Get item history
      *
-     * @return null|string
+     * @return string
      */
     public function getItemHistory()
     {
@@ -670,7 +670,7 @@ class SolrEad3 extends SolrEad
                 }
             }
         }
-        return null;
+        return '';
     }
 
     /**
@@ -1891,7 +1891,7 @@ class SolrEad3 extends SolrEad
                         $lang = $this->detectNodeLanguage($p);
                         $url = isset($p->ref)
                             ? (string)$p->ref->attributes()->href : null;
-                        if ($this->urlBlocked($url, $text)) {
+                        if ($url && $this->urlBlocked($url, $text)) {
                             $url = null;
                         }
                         $data = compact('text', 'lang', 'url');
