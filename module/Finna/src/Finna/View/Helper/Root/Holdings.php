6d279d5db22346b2efefe9ca8d1ba17de13b3013
Ere Maijala <ere.maijala@helsinki.fi>
2022-10-07 14:16:36 +0300

    Upstream compatibility: Make Finna Holdings view helper descend from VuFind's.

--- module/Finna/src/Finna/View/Helper/Root/Holdings.php
+++ module/Finna/src/Finna/View/Helper/Root/Holdings.php
@@ -4,7 +4,7 @@
  *
  * PHP version 7
  *
- * Copyright (C) The National Library of Finland 2015-2020.
+ * Copyright (C) The National Library of Finland 2015-2022.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2,
@@ -28,8 +28,6 @@
  */
 namespace Finna\View\Helper\Root;
 
-use Laminas\View\Helper\AbstractHelper;
-
 /**
  * Holdings Settings Helper
  *
@@ -40,25 +38,8 @@ use Laminas\View\Helper\AbstractHelper;
  * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
  * @link     http://vufind.org/wiki/vufind2:developer_manual Wiki
  */
-class Holdings extends AbstractHelper
+class Holdings extends \VuFind\View\Helper\Root\Holdings
 {
-    /**
-     * VuFind configuration
-     *
-     * @var \Laminas\Config\Config
-     */
-    protected $config;
-
-    /**
-     * Constructor
-     *
-     * @param \Laminas\Config\Config $config VuFind configuration
-     */
-    public function __construct(\Laminas\Config\Config $config = null)
-    {
-        $this->config = $config;
-    }
-
     /**
      * Return the configured holding details mode
      *
@@ -66,9 +47,9 @@ class Holdings extends AbstractHelper
      */
     public function getDetailsMode()
     {
-        return empty($this->config->Record['holdings_details'])
+        return empty($this->config['Record']['holdings_details'])
             ? 'expand-first'
-            : $this->config->Record['holdings_details'];
+            : $this->config['Record']['holdings_details'];
     }
 
     /**
@@ -78,8 +59,7 @@ class Holdings extends AbstractHelper
      */
     public function getCollapseThreshold()
     {
-        return empty($this->config->Item_Status->collapse_threshold)
-            ? null : $this->config->Item_Status->collapse_threshold->toArray();
+        return $this->config['Item_Status']['collapse_threshold'] ?? null;
     }
 
     /**
@@ -87,79 +67,70 @@ class Holdings extends AbstractHelper
      * group title so that they can be seen without "expanding" the group.
      * This only affects record page holdings.
      *
-     * @return boolean
+     * @return bool
      */
     public function showDetailsAfterLocationGroup()
     {
-        return empty($this->config->Item_Status->show_details_after_location_group)
-            ? false
-            : (bool)$this->config->Item_Status->show_details_after_location_group;
+        $result = $this->config['Item_Status']['show_details_after_location_group']
+            ?? false;
+        return (bool)$result;
     }
 
     /**
-     * Return configured option for showing
-     * link to record page in search results holdings.
+     * Return configured option for showing link to record page in search results
+     * holdings.
      *
-     * @return boolean
+     * @return bool
      */
     public function showLinkToRecordPage()
     {
-        return empty($this->config->Item_Status->show_link_to_record_page)
-            ? false
-            : (bool)$this->config->Item_Status->show_link_to_record_page;
+        $result = $this->config['Item_Status']['show_link_to_record_page'] ?? false;
+        return (bool)$result;
     }
 
     /**
-     * Return configured option for showing
-     * place hold button link in search results holdings.
+     * Return configured option for showing place hold button link in search results
+     * holdings.
      *
-     * @return boolean
+     * @return bool
      */
     public function showSearchResultsTitleHold()
     {
-        return empty($this->config->Item_Status->show_title_hold)
-            ? false
-            : (bool)$this->config->Item_Status->show_title_hold;
+        return (bool)($this->config['Item_Status']['show_title_hold'] ?? false);
     }
 
     /**
-     * Return configured setting for showing
-     * holdings summariy on record page.
+     * Return configured setting for showing holdings summariy on record page.
      *
-     * @return boolean
+     * @return bool
      */
     public function showRecordPageSummary()
     {
-        return empty($this->config->Item_Status->show_holdings_summary)
-            ? false
-            : (bool)$this->config->Item_Status->show_holdings_summary;
+        $result = $this->config['Item_Status']['show_holdings_summary'] ?? false;
+        return (bool)$result;
     }
 
     /**
-     * Return configured setting for overriding holdings ordering and
-     * re-sorting holdings by availability and location name. This only affects
-     * non-Axiell search results holdings.
+     * Return configured setting for overriding holdings ordering and re-sorting
+     * holdings by availability and location name. This only affects non-Axiell
+     * search results holdings.
      *
-     * @return boolean
+     * @return bool
      */
     public function overrideSortOrder()
     {
-        return empty($this->config->Item_Status->override_sort_order)
-            ? false
-            : (bool)$this->config->Item_Status->override_sort_order;
+        return (bool)($this->config['Item_Status']['override_sort_order'] ?? false);
     }
 
     /**
      * Return configured limit for truncating holdings. This only affects non-Axiell
      * search results holdings.
      *
-     * @return boolean
+     * @return int
      */
     public function getTruncateLimit()
     {
-        return !isset($this->config->Item_Status->truncate_limit)
-            ? false
-            : $this->config->Item_Status->truncate_limit;
+        return (int)($this->config['Item_Status']['truncate_limit'] ?? 0);
     }
 
     /**
