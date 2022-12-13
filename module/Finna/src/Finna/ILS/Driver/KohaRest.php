c2dcb72f2feb38a6690282974b3039ba60adbc3c
Ere Maijala <ere.maijala@helsinki.fi>
2022-12-13 09:46:38 +0200

    KohaRest: Add a workaround for Koha 22.11 BC break.
    Bug 30275 changed the renewals field of checkouts to renewals_count, so we need to account for both.

--- module/Finna/src/Finna/ILS/Driver/KohaRest.php
+++ module/Finna/src/Finna/ILS/Driver/KohaRest.php
@@ -1963,7 +1963,9 @@ class KohaRest extends \VuFind\ILS\Driver\KohaRest
             }
 
             $renewable = $entry['renewable'];
-            $renewals = $entry['renewals'];
+            // Koha 22.11 introduced a backward compatibility break by renaming
+            // renewals to renewals_count (bug 30275), so check both:
+            $renewals = $entry['renewals_count'] ?? $entry['renewals'];
             $renewLimit = $entry['max_renewals'];
             $message = '';
             if (!$renewable && !$checkedIn) {
