ca1cef6cdd15b38087d565c77dd15e83db439816
Ere Maijala <ere.maijala@helsinki.fi>
2023-01-13 14:10:41 +0200

    [ALLI-6203] Move user data source preferences to a service. (#2493)
    Also fixes source priority determination when sources are not set in searches.ini.

---
+++ module/Finna/src/Finna/Service/UserPreferenceService.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * User preference support service
+ *
+ * PHP version 7
+ *
+ * Copyright (C) The National Library of Finland 2023.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @category VuFind
+ * @package  Service
+ * @author   Ere Maijala <ere.maijala@helsinki.fi>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     http://vufind.org/wiki/vufind2:developer_manual Wiki
+ */
+namespace Finna\Service;
+
+use VuFind\Auth\Manager as AuthManager;
+use VuFind\Cookie\CookieManager;
+
+/**
+ * User preference support service
+ *
+ * @category VuFind
+ * @package  Service
+ * @author   Ere Maijala <ere.maijala@helsinki.fi>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     http://vufind.org/wiki/vufind2:developer_manual Wiki
+ */
+class UserPreferenceService
+{
+    /**
+     * Authentication manager
+     *
+     * @var AuthManager
+     */
+    protected $authManager;
+
+    /**
+     * Cookie manager
+     *
+     * @var ?CookieManager
+     */
+    protected $cookieManager;
+
+    /**
+     * Constructor.
+     *
+     * @param AuthManager    $authManager   Authentication manager
+     * @param ?CookieManager $cookieManager Cookie manager
+     */
+    public function __construct(
+        AuthManager $authManager,
+        CookieManager $cookieManager = null
+    ) {
+        $this->authManager = $authManager;
+        $this->cookieManager = $cookieManager;
+    }
+
+    /**
+     * Get preferred data sources in order of importance for a user
+     *
+     * @return array
+     */
+    public function getPreferredDataSources(): array
+    {
+        $result = [];
+
+        // preferredRecordSource cookie defines the primary sources:
+        if ($this->cookieManager) {
+            $preferred = $this->cookieManager->get('preferredRecordSource');
+            if (!empty($preferred)) {
+                // Check if the cookie is a JSON array:
+                if ('[' === mb_substr($preferred, 0, 1)) {
+                    $preferred = json_decode($preferred);
+                    if (is_array($preferred)) {
+                        $result = $preferred;
+                    }
+                } else {
+                    $result = [$preferred];
+                }
+            }
+        }
+
+        // Selected library card is used as secondary/fallback:
+        if ($user = $this->authManager->isLoggedIn()) {
+            if ($user->cat_username) {
+                [$source] = explode('.', $user->cat_username, 2);
+                $result[] = $source;
+            }
+        }
+
+        return $result;
+    }
+}
