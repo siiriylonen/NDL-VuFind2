99f68364ff91cbed8688f5cb5ca0818b1ce799a1
rajaro <jaro.ravila@gmail.com>
2023-01-24 13:27:35 +0200

    [ALLI-7186] Add sort options to holds list (#2439)

--- module/Finna/src/Finna/Controller/HoldsController.php
+++ module/Finna/src/Finna/Controller/HoldsController.php
@@ -59,12 +59,98 @@ class HoldsController extends \VuFind\Controller\HoldsController
         if ($view = $this->createViewIfUnsupported('getMyHolds')) {
             return $view;
         }
-
         $view = parent::listAction();
         if (isset($view->recordList)) {
-            $view->recordList = $this->orderAvailability($view->recordList);
+            $sort = $this->params()->fromQuery('sort', 'available');
+            $supported = [
+                'expire_asc',
+                'expire_desc',
+                'create_asc',
+                'create_desc',
+                'title_desc'
+            ];
+            if (in_array($sort, $supported)) {
+                $view->recordList = $this->sortHolds($view->recordList, $sort);
+            } else {
+                $view->recordList = $this->orderAvailability($view->recordList);
+            }
+            $sortList = [
+                'available' => [
+                    'desc' =>  'hold_sort_available',
+                    'url' => '?sort=available',
+                    'selected' => $sort === 'available'
+                ],
+                'expire_asc' => [
+                    'desc' =>  'hold_sort_expire_asc',
+                    'url' => '?sort=expire_asc',
+                    'selected' => $sort === 'expire_asc'
+                ],
+                'expire_desc' => [
+                    'desc' =>  'hold_sort_expire_desc',
+                    'url' => '?sort=expire_desc',
+                    'selected' => $sort === 'expire_desc'
+                ],
+                'create_asc' => [
+                    'desc' =>  'hold_sort_create_asc',
+                    'url' => '?sort=create_asc',
+                    'selected' => $sort === 'create_asc'
+                ],
+                'create_desc' => [
+                    'desc' =>  'hold_sort_create_desc',
+                    'url' => '?sort=create_desc',
+                    'selected' => $sort === 'create_desc'
+                ],
+                'title_desc' => [
+                    'desc' => 'hold_sort_title',
+                    'url' => '?sort=title_desc',
+                    'selected' => $sort === 'title_desc'
+                ]
+            ];
+            $view->sortList = $sortList;
         }
         $view->blocks = $this->getAccountBlocks($patron);
         return $view;
+    }
+
+    /**
+     * Sort holds list
+     *
+     * @param array  $recordList array of holds
+     * @param string $sort       sort order
+     *
+     * @return array
+     */
+    protected function sortHolds($recordList, $sort)
+    {
+        [$field, $order] = explode('_', $sort);
+        $date = $this->serviceLocator->get(\VuFind\Date\Converter::class);
+        $sorter = $this->serviceLocator->get(\VuFind\I18n\Sorter::class);
+        $sortFunc = function ($a, $b) use ($field, $order, $date, $sorter) {
+            $aDetail = $a->getExtraDetail('ils_details')[$field] ?? '';
+            $bDetail = $b->getExtraDetail('ils_details')[$field] ?? '';
+            if ($field === 'title') {
+                return $sorter->compare(
+                    $aDetail,
+                    $bDetail
+                );
+            }
+            $aDate = $aDetail ? $date->convertFromDisplayDate('U', $aDetail) : 0;
+            $bDate = $bDetail ? $date->convertFromDisplayDate('U', $bDetail) : 0;
+            if ($aDetail !== $bDetail) {
+                return $order === 'asc' ? $aDate - $bDate : $bDate - $aDate;
+            }
+            $aAvail = $a->getExtraDetail('ils_details')['available'] ?? '';
+            $bAvail = $b->getExtraDetail('ils_details')['available'] ?? '';
+            if ($aAvail !== $bAvail) {
+                return (int)$bAvail - (int)$aAvail;
+            }
+            return $sorter->compare(
+                $a->getExtraDetail('ils_details')['title'] ?? '',
+                $b->getExtraDetail('ils_details')['title'] ?? ''
+            );
+        };
+
+        usort($recordList, $sortFunc);
+        return $recordList;
     }
 }
