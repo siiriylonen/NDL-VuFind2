b7f132935deda2279a84200ee814810841b3b775
Siiri Ylönen <siiri.ylonen@helsinki.fi>
2023-03-03 12:56:32 +0200

    Reverted last js change.

--- themes/finna2/js/finna-record.js
+++ themes/finna2/js/finna-record.js
@@ -29,17 +29,16 @@ finna.record = (function finnaRecord() {
           description.hide();
         });
     }
-    const button = $('.show-hide-button');
-    const more = button.html();
+    const more = $('.show-hide-button').html();
     const less = $('.hide-info').html();
     $('.cc-info').on('show.bs.collapse', function changeText() {
-      $(this).parents('.fulltextField').find(button).html(less);
+      $(this).parents('.fulltextField').find('.show-hide-button').html(less);
     }).on('hidden.bs.collapse', function changeText() {
-      $(this).parents('.fulltextField').find(button).html(more);
+      $(this).parents('.fulltextField').find('.show-hide-button').html(more);
     });
     $('.hide-info').on('click', function handleClick() {
       $(this).blur();
-      $(this).parents('.fulltextField').find(button).focus();
+      $(this).parents('.fulltextField').find('.show-hide-button').focus();
     });
   }
 
