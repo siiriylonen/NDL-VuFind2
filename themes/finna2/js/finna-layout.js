c6ef51745a966c84155feccb903e18f5d259f7b8
MillaKPE <130566695+MillaKPE@users.noreply.github.com>
2023-06-01 11:02:55 +0300

    [ALLI-7698] Make less button sticky (take two) (#2621)

--- themes/finna2/js/finna-layout.js
+++ themes/finna2/js/finna-layout.js
@@ -115,13 +115,12 @@ finna.layout = (function finnaLayout() {
 
         if (self.data('button-placement') === 'top') {
           self.before([moreLink, lessLink]);
+        } else if (topLink) {
+          self.before(lessLink.addClass('top-button'));
+          self.after([moreLink]);
         } else {
-          if (topLink) {
-            self.before(lessLink.clone(true));
-          }
           self.after([moreLink, lessLink]);
         }
-        self.addClass('truncated');
       }
     });
   }
