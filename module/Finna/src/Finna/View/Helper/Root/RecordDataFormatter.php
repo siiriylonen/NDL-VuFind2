25c476f6f979123975e2e1a192023129c96c3af9
Juha Luoma <33253757+LuomaJuha@users.noreply.github.com>
2023-09-15 12:56:05 +0300

    [FINNA-1037] Check for driver in RecordDataFormatter (#2690)

--- module/Finna/src/Finna/View/Helper/Root/RecordDataFormatter.php
+++ module/Finna/src/Finna/View/Helper/Root/RecordDataFormatter.php
@@ -695,8 +695,8 @@ class RecordDataFormatter extends \VuFind\View\Helper\Root\RecordDataFormatter
     {
         $intersected = array_intersect_key($coreFields, array_flip($include));
         $config = $this->getView()->plugin('config')->get('datasources');
-        $source = $this->driver->tryMethod('getSource');
-        if ($source && $hide = $config->$source->hidden_record_fields ?? []) {
+        $source = $this->driver?->tryMethod('getSource');
+        if ($source && $hide = $config->$source?->hidden_record_fields) {
             $intersected = array_diff_key($intersected, array_flip($hide->toArray()));
         }
         return $intersected;
