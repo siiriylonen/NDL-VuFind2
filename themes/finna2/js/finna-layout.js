b6565af5fd831811f55931cdf9ed2e6e1da42ad9
siiriylonen <117457514+siiriylonen@users.noreply.github.com>
2024-03-14 14:06:26 +0200

    [FINNA-1006] Fix tooltip accessiblity (#2873)

--- themes/finna2/js/finna-layout.js
+++ themes/finna2/js/finna-layout.js
@@ -272,6 +272,7 @@ finna.layout = (function finnaLayout() {
     holder.find('[data-toggle="tooltip"]')
       .on('show.bs.tooltip', function onShowTooltip() {
         var self = $(this);
+        $(this).attr('aria-expanded', 'true');
         $(currentOpenTooltips).each(function hideOtherTooltips() {
           if ($(this)[0] !== self[0]) {
             $(this).tooltip('hide');
@@ -280,9 +281,11 @@ finna.layout = (function finnaLayout() {
         currentOpenTooltips = [self];
       })
       .on('hidden.bs.tooltip', function onHideTooltip(e) {
+        $(e.target).attr('aria-expanded', 'false');
         $(e.target).data('bs.tooltip').inState.click = false;
       })
-      .tooltip({trigger: 'click', viewport: '.container'});
+      .tooltip({trigger: 'click', viewport: '.container'})
+      .attr('aria-expanded', 'false');
 
     holder.find('[data-toggle="tooltip-hover')
       .tooltip({trigger: 'hover', delay: {show: 1000, hide: 200}});
@@ -293,6 +296,13 @@ finna.layout = (function finnaLayout() {
     // close tooltip if user clicks anything else than tooltip button
     $('html').on('click', function onClickHtml(e) {
       if (typeof $(e.target).parent().data('original-title') == 'undefined' && typeof $(e.target).data('original-title') == 'undefined') {
+        $('[data-toggle="tooltip"]').tooltip('hide');
+        currentOpenTooltips = [];
+      }
+    });
+    // close tooltip if esc-key pressed
+    $('html').on('keydown', function onClickHtml(e) {
+      if (e.which === 27) {
         $('[data-toggle="tooltip"]').tooltip('hide');
         currentOpenTooltips = [];
       }
