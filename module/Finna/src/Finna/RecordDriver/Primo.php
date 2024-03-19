79b22dad5198f2e370934cdc4a78b408ae9bc96c
Ere Maijala <ere.maijala@helsinki.fi>
2024-03-19 14:16:44 +0200

    Primo: Cast urls to array to ensure proper iteration.

--- module/Finna/src/Finna/RecordDriver/Primo.php
+++ module/Finna/src/Finna/RecordDriver/Primo.php
@@ -166,7 +166,7 @@ class Primo extends \VuFind\RecordDriver\Primo
             if (!($urls = $this->fields['resource_urls'][$link] ?? [])) {
                 continue;
             }
-            foreach ($urls as $current) {
+            foreach ((array)$urls as $current) {
                 if (is_array($current)) {
                     $desc = $current['label'];
                     $url = $current['url'];
