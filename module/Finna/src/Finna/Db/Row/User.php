f2f04dbd9c8fd76582882d4d7be9439f30538d6b
Ere Maijala <ere.maijala@helsinki.fi>
2024-05-16 10:16:48 +0300

    Fix RateLimiter for logged-in users.

--- module/Finna/src/Finna/Db/Row/User.php
+++ module/Finna/src/Finna/Db/Row/User.php
@@ -234,4 +234,14 @@ class User extends \VuFind\Db\Row\User
             ]
         );
     }
+
+    /**
+     * Get identifier (returns null for an uninitialized or non-persisted object).
+     *
+     * @return ?int
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
 }
